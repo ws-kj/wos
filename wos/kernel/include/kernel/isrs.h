26abd646f7dcf309bf21a41da8592df7e6166cd4
Will Savage <wsavage6316@gmail.com>
2019-05-26 17:50:21 -0400

    ISRs work now, but don't printf properly

---
+++ wos/kernel/include/kernel/isrs.h
@@ -0,0 +1,15 @@
+#ifndef KERNEL_ISRS_H
+#define KERNEL_ISRS_H
+
+extern void isrs_install();
+
+/* This defines what the stack looks like after an ISR was running */
+struct regs
+{
+    unsigned int gs, fs, es, ds;
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    unsigned int int_no, err_code;
+    unsigned int eip, cs, eflags, useresp, ss;    
+};
+
+#endif
--- wos/kernel/kernel/kernel.c
+++ wos/kernel/kernel/kernel.c
@@ -3,12 +3,16 @@
 #include <kernel/tty.h>
 #include <kernel/gdt.h>
 #include <kernel/idt.h>
+#include <kernel/isrs.h>
 
 void kernel_main(void) {
 	gdt_install();
 	idt_install();
+	isrs_install();
 	terminal_initialize();
 
 	printf("wos v0.1\n");
 	printf("Hello, kernel World!\n");
+	//test exception
+	printf("%d", (1/0));
 }
