864e82af2344d01d7a4e54115a45ac2643cd64c8
Will Savage <wsavage6316@gmail.com>
2019-05-26 10:59:31 -0400

    Added basic IDT

--- wos/kernel/kernel/kernel.c
+++ wos/kernel/kernel/kernel.c
@@ -2,10 +2,13 @@
 
 #include <kernel/tty.h>
 #include <kernel/gdt.h>
+#include <kernel/idt.h>
 
 void kernel_main(void) {
 	gdt_install();
+	idt_install();
 	terminal_initialize();
+
 	printf("wos v0.1\n");
 	printf("Hello, kernel World!\n");
 }
