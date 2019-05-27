00bb1e8190a75ddb5bb76826ed58a9365cacc3c3
Will Savage <wsavage6316@gmail.com>
2019-05-26 22:25:53 -0400

    IRQ works? I think? TBT

--- wos/kernel/kernel/kernel.c
+++ wos/kernel/kernel/kernel.c
@@ -4,11 +4,16 @@
 #include <kernel/gdt.h>
 #include <kernel/idt.h>
 #include <kernel/isrs.h>
+#include <kernel/irq.h>
 
 void kernel_main(void) {
 	gdt_install();
 	idt_install();
 	isrs_install();
+
+	irq_install();
+	__asm__ __volatile__ ("sti");
+
 	terminal_initialize();
 
 	printf("wos v0.01\n");
---
+++ wos/sysroot/usr/include/kernel/irq.h
@@ -0,0 +1,6 @@
+#ifndef KERNEL_IRQ_H
+#define	KERNEL_IRQ_H
+
+extern void irq_install();
+
+#endif
