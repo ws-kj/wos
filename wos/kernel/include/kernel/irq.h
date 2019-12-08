0f286adeb60484284533c291c6ac3780964e6a6f
Will Savage <wsavage6316@gmail.com>
2019-12-08 16:16:13 -0500

    Fixed irq header error

--- wos/kernel/include/kernel/irq.h
+++ wos/kernel/include/kernel/irq.h
@@ -1,6 +1,8 @@
 #ifndef KERNEL_IRQ_H
 #define	KERNEL_IRQ_H
 
+struct regs;
+
 extern void irq_install();
 void irq_install_handler(int irq, void (*handler)(struct regs *r));
 extern void irq_uninstall_handler(int irq);
