c66a5346a6e1bdb37547b8057656b9d1ae31aeb6
Will Savage <wsavage6316@gmail.com>
2019-12-15 12:26:12 -0500

    removed paging

--- src/main.c
+++ src/main.c
@@ -1,19 +1,12 @@
 #include "common.h"
 #include "monitor.h"
 #include "descriptor_tables.h"
-#include "paging.h"
 #include "timer.h"
 
 int kernel_main(struct multiboot *mboot_ptr) {
 	init_descriptor_tables();
 	monitor_clear();
-	initialise_paging();
 	monitor_write("wos v0.01\n");
-
-  	uint32_t *ptr = (uint32_t*)0xA0000000;
-   	uint32_t do_page_fault = *ptr;
-
-
 
 	for(;;) {
     		__asm__ __volatile__("hlt");
