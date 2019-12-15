f269ed0d64fc09c04cdd4089b30d9cb4434073e8
Will Savage <wsavage6316@gmail.com>
2019-12-15 14:11:10 -0500

    added basic keyboard in

--- src/build.sh
+++ src/build.sh
@@ -9,7 +9,8 @@ i686-elf-gcc -c monitor.c -o monitor.o -ffreestanding -std=gnu99 -O2 -Wall -Wext
 i686-elf-gcc -c descriptor_tables.c -o descriptor_tables.o -ffreestanding -std=gnu99 -O2 -Wall -Wextra
 i686-elf-gcc -c isr.c -o isr.o -ffreestanding -std=gnu99 -O2 -Wall -Wextra
 i686-elf-gcc -c timer.c -o timer.o -ffreestanding -std=gnu99 -O2 -Wall -Wextra
+i686-elf-gcc -c keyboard.c -o keyboard.o -ffreestanding -std=gnu99 -O2 -Wall -Wextra
 i686-elf-gcc -c kheap.c -o kheap.o -ffreestanding -std=gnu99 -O2 -Wall -Wextra
 i686-elf-gcc -c main.c -o main.o -ffreestanding -std=gnu99 -O2 -Wall -Wextra
-i686-elf-gcc -T link.ld -o kernel.bin -ffreestanding -O2 -nostdlib boot.o main.o common.o monitor.o interrupt.o descriptor_tables.o gdt.o isr.o timer.o kheap.o -lgcc
+i686-elf-gcc -T link.ld -o kernel.bin -ffreestanding -O2 -nostdlib boot.o main.o common.o monitor.o interrupt.o descriptor_tables.o gdt.o isr.o timer.o kheap.o keyboard.o -lgcc
 
--- src/main.c
+++ src/main.c
@@ -2,10 +2,12 @@
 #include "monitor.h"
 #include "descriptor_tables.h"
 #include "timer.h"
+#include "keyboard.h"
 
 int kernel_main(struct multiboot *mboot_ptr) {
 	init_descriptor_tables();
 	init_timer(50);
+	init_keyboard();
 	monitor_clear();
 	monitor_write("wos v0.01\n");
 
